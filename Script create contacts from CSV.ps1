a975402d4f981b63e38c45245262b9a8b3498459
Uellington <uellington@gmail.com>
2020-04-17 10:41:43 -0300

    Versão publica

--- Script create contacts from CSV.ps1
+++ Script create contacts from CSV.ps1
@@ -1,17 +1,20 @@
+#Programador: Uellington
+#Data: 17/04/2020
+#Versão: 1.0
 
 clear-host
-$mails = Import-Csv -encoding UTF8 "C:\Temp\Contatos-Cemig.csv"
+$mails = Import-Csv -encoding UTF8 "C:\Temp\Contatos.csv"
 clear-host
 foreach ($mail in $mails)
     {
         $name = $mail.DisplayName
         $SMTP = $mail.PrimarySmtpAddress
-        $nameAD = Get-ADObject -Filter * -SearchBase "CN=$name,OU=CEMIG,OU=CONTACTS,OU=OFFICE365,OU=UsuariosForluz,OU=FORLUZ,DC=forluznet,DC=net" -Properties *
+        $nameAD = Get-ADObject -Filter * -SearchBase "CN=$name,OU=CLIENTE,OU=CONTACTS,OU=OFFICE365,OU=UsersEmpresa,OU=empresa,DC=empresa,DC=net" -Properties *
         if ( $nameAD.DisplayName -eq $name)
             {
                 $mail.DisplayName >> C:\temp\naocriados.txt
             }else{
-                New-ADObject -type contact -path "OU=CEMIG,OU=CONTACTS,OU=OFFICE365,OU=UsuariosForluz,OU=FORLUZ,DC=forluznet,DC=net"  -Name $name -otherAttributes @{'displayName'=$name;'mail'=$SMTP;'extensionAttribute10'='CEMIG_SA'}
+                New-ADObject -type contact -path "OU=CLIENTE,OU=CONTACTS,OU=OFFICE365,OU=UsersEMPRESA,OU=EMPRESA,DC=empresa,DC=net"  -Name $name -otherAttributes @{'displayName'=$name;'mail'=$SMTP;'extensionAttribute10'='ATRIBUTO'}
                 $mail.DisplayName >> C:\temp\criados.txt
             }
     }
